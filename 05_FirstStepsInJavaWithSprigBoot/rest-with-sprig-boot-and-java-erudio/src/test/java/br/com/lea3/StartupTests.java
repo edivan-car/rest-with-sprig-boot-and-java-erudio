63510b726c6bbb6ed8ca88526ab70dbf9fffb6b5
edivan-car <edivam.c@gmail.com>
2025-01-24 15:13:00 -0300

    First Steps in Java with Spring Boot

---
+++ 05_FirstStepsInJavaWithSprigBoot/rest-with-sprig-boot-and-java-erudio/src/test/java/br/com/lea3/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.lea3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
